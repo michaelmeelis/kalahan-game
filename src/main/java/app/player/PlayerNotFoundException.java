9627bb206cb6f2b2f4c9e133e6a9abc7eec0fe52
Michael Meelis <michael.meelis@nepworldwide.com>
2017-11-02 00:37:28 +0100

    Created a player not found exception

---
+++ src/main/java/app/player/PlayerNotFoundException.java
@@ -0,0 +1,10 @@
+package app.player;
+
+/**
+ * Created by michaelmeelis on 31/10/2017.
+ */
+public class PlayerNotFoundException extends Exception {
+    public PlayerNotFoundException(String message) {
+        super(message);
+    }
+}
